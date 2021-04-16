8f71c10bf92c5d743b4eb12022bb8f774a441da0
Juan Trejo <jtrejo4545@gmail.com>
2021-04-16 01:45:00 -0500

    added components

---
+++ src/Components/FooterComponent.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function FooterComponent() {
+    return (
+        <div className="footer">
+            <div className="container">
+                <div className="row justify-content-center">
+                    <div className="col-md-12">
+                    <a href="https://github.com/jt113" className="App-link">GitHub</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    );
+}
+export default FooterComponent;
